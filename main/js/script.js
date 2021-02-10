159665739b6dbec15c8d25f6be2c4b0c7bca0a3a
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2021-02-10 12:58:05 +0300

    d

--- main/js/script.js
+++ main/js/script.js
@@ -15,6 +15,7 @@ $(".slider").slick({
   prevArrow: ".slider__pre",
   nextArrow: ".slider__next",
 });
+
 const items = document.querySelectorAll(".slider__img");
 const phone = document.querySelectorAll(".img__media");
 const popup = document.querySelectorAll(".slider__popup");
@@ -28,7 +29,7 @@ console.log(phone);
 //               item.classList.add("img__media-active");
 //             } else {
 //               item.classList.remove("img__media-active");
-              
+
 //             }
 //             if(item.classList.contains("img__media-active")){
 //                 itm.classList.add("slider__popup-active");
@@ -41,58 +42,62 @@ console.log(phone);
 //   });
 // });
 slider__item1.addEventListener("click", () => {
-    if (!img__media1.classList.contains("img__media-active")) {
-        img__media1.classList.add("img__media-active");
-      } else {
-        img__media1.classList.remove("img__media-active");
-        
-      }
-      if(img__media1.classList.contains("img__media-active")){
-        slider__popup1.classList.add("slider__popup-active");
-      }
-      else{
-        slider__popup1.classList.remove("slider__popup-active");
-      }
-})
+  if (!img__media1.classList.contains("img__media-active")) {
+    img__media1.classList.add("img__media-active");
+  } else {
+    img__media1.classList.remove("img__media-active");
+  }
+  if (img__media1.classList.contains("img__media-active")) {
+    slider__popup1.classList.add("slider__popup-active");
+  } else {
+    slider__popup1.classList.remove("slider__popup-active");
+  }
+});
 slider__item2.addEventListener("click", () => {
-    if (!img__media2.classList.contains("img__media-active")) {
-        img__media2.classList.add("img__media-active");
-      } else {
-        img__media2.classList.remove("img__media-active");
-        
-      }
-      if(img__media2.classList.contains("img__media-active")){
-        slider__popup2.classList.add("slider__popup-active");
-      }
-      else{
-        slider__popup2.classList.remove("slider__popup-active");
-      }
-})
+  if (!img__media2.classList.contains("img__media-active")) {
+    img__media2.classList.add("img__media-active");
+  } else {
+    img__media2.classList.remove("img__media-active");
+  }
+  if (img__media2.classList.contains("img__media-active")) {
+    slider__popup2.classList.add("slider__popup-active");
+  } else {
+    slider__popup2.classList.remove("slider__popup-active");
+  }
+});
 slider__item3.addEventListener("click", () => {
-    if (!img__media3.classList.contains("img__media-active")) {
-        img__media3.classList.add("img__media-active");
-      } else {
-        img__media3.classList.remove("img__media-active");
-        
-      }
-      if(img__media3.classList.contains("img__media-active")){
-        slider__popup3.classList.add("slider__popup-active");
-      }
-      else{
-        slider__popup3.classList.remove("slider__popup-active");
-      }
-})
+  if (!img__media3.classList.contains("img__media-active")) {
+    img__media3.classList.add("img__media-active");
+  } else {
+    img__media3.classList.remove("img__media-active");
+  }
+  if (img__media3.classList.contains("img__media-active")) {
+    slider__popup3.classList.add("slider__popup-active");
+  } else {
+    slider__popup3.classList.remove("slider__popup-active");
+  }
+});
 slider__item4.addEventListener("click", () => {
-    if (!img__media4.classList.contains("img__media-active")) {
-        img__media4.classList.add("img__media-active");
-      } else {
-        img__media4.classList.remove("img__media-active");
-        
-      }
-      if(img__media4.classList.contains("img__media-active")){
-        slider__popup4.classList.add("slider__popup-active");
-      }
-      else{
-        slider__popup4.classList.remove("slider__popup-active");
-      }
-})
+  if (!img__media4.classList.contains("img__media-active")) {
+    img__media4.classList.add("img__media-active");
+  } else {
+    img__media4.classList.remove("img__media-active");
+  }
+  if (img__media4.classList.contains("img__media-active")) {
+    slider__popup4.classList.add("slider__popup-active");
+  } else {
+    slider__popup4.classList.remove("slider__popup-active");
+  }
+});
+
+$(".slider").on(
+  "afterChange",
+  function (event, slick, currentSlide, nextSlide) {
+    phone.forEach((item) => {
+      item.classList.remove("img__media-active");
+    });
+    popup.forEach((item) => {
+      item.classList.remove("slider__popup-active");
+    });
+  }
+);
